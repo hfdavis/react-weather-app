c911e55089715d27c3d00358428769a3e9e720bd
hfdavis <149373126+hfdavis@users.noreply.github.com>
2024-03-01 13:54:35 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was created by Holly Davis ans is open sourced on{" "}
+        <a href="https://github.com/hfdavis/react-weather-app" target="blank">
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
